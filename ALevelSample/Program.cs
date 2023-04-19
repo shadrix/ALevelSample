2b9dbb02de1d0b1f6aabde7e8a6039b33c091fa2
shadrix <postilha.dmytro@gmail.com>
2023-04-19 20:51:46 +0300

    feat: added semaphoreslim with write to file

--- ALevelSample/Program.cs
+++ ALevelSample/Program.cs
@@ -1,3 +1,38 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
-Console.WriteLine("Hello, World!");
+var streamWriter = new StreamWriter("text");
+var semaphoreSlim = new SemaphoreSlim(1);
+
+Task.Run(() =>
+{
+    for (var i = 0; i < 1000; i++)
+    {
+        var k = i;
+        Task.Run(async () => { await WriteAsync(k.ToString()); });
+    }
+});
+
+Task.Run(() =>
+{
+    for (var i = 2000; i < 3000; i++)
+    {
+        var k = i;
+        Task.Run(async () => { await WriteAsync(k.ToString()); });
+    }
+});
+
+Console.ReadKey();
+
+async Task WriteAsync(string text)
+{
+    await semaphoreSlim.WaitAsync();
+
+    await streamWriter.WriteLineAsync(text);
+    await streamWriter
+        .FlushAsync(); // https://docs.microsoft.com/en-us/dotnet/api/system.io.stream.flushasync?view=net-5.0
+
+    semaphoreSlim.Release();
+}
